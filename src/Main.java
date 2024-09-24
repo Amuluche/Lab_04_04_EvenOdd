9396eb08093c1dc2c34fd5b5bf468b656a98028d
Victor Amuluche <amulucvc@mail.uc.edu>
2024-09-23 21:11:08 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int numToExamine = 2;
+        int EvenOdd= numToExamine % numToExamine;
+    System.out.println("your number " + EvenOdd + " is even");
+    }
+}
